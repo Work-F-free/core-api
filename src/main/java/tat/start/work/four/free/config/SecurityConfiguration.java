8cc2c1d20b738025d76eed71ea61b3eb829e1e55
kamil_badrutdinov <kamil.badrutdinov@glowbyteconsulting.com>
2025-02-07 16:39:41 +0300

    security disabled

--- src/main/java/tat/start/work/four/free/config/SecurityConfiguration.java
+++ src/main/java/tat/start/work/four/free/config/SecurityConfiguration.java
@@ -15,32 +15,32 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    CorsConfigurationSource configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
-        return urlBasedCorsConfigurationSource;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(c -> c.configurationSource(configurationSource()))
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll());
+//    @Bean
+//    CorsConfigurationSource configurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("*"));
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+//        return urlBasedCorsConfigurationSource;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .cors(c -> c.configurationSource(configurationSource()))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().permitAll());
 //                        .authenticated());
 
 //        http.oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
-
-        return http.build();
-    }
+//
+//        return http.build();
+//    }
 
 }
